b9b8e4b0096585306769964a87db97094dcb2218
Marwane Assou <marwane.assou@gmail.com>
2024-02-04 21:35:36 +0100

    missing number

---
+++ missing_number/src/missing_number/Main.java
@@ -0,0 +1,14 @@
+package missing_number;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] arr= {1,3,5,4,6,8,9,7,10};
+		int n=arr.length+1;
+		int sum=(n*(n+1))/2;
+		for(int number:arr) {
+			sum-=number;
+		}
+		System.out.println(sum);
+		
+	}
+}
---
+++ missing_number/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module missing_number {
+}
