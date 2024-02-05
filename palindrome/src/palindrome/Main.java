274c799a1a4537c2896c7ceed5e0d12b48d54bd9
Marwane Assou <marwane.assou@gmail.com>
2024-02-05 10:54:52 +0100

    palindrome String

--- palindrome/src/palindrome/Main.java
+++ palindrome/src/palindrome/Main.java
@@ -2,12 +2,13 @@ package palindrome;
 
 public class Main {
 	public static void main(String[] args) {
-		String str="ooooooooooooooooooooooooooooooooooooooo";
+		String str="madam";
+		str=str.replace(" ", "");
 		int count=0;
 		for(int i=0,j=str.length()-1;i<j;i++,j--) {
 			if(str.charAt(i)==str.charAt(j))
 				count++;
 		}
-		System.out.println(count==str.length()/2 ? true : false);
+		System.out.println(count==str.length()/2);
 	}
 }
