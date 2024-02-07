e9112e38eb91871b53df1f634a73985c6c3d5790
Marwane Assou <marwane.assou@gmail.com>
2024-02-07 23:02:11 +0100

    insert node in a sorted singly linked list

--- insert_node_in_sorted_linkedList/src/insert_node_in_sorted_linkedList/Main.java
+++ insert_node_in_sorted_linkedList/src/insert_node_in_sorted_linkedList/Main.java
@@ -1,29 +1,31 @@
 package insert_node_in_sorted_linkedList;
 
-import insert_node_in_sorted_linkedList.SinglyLinkedList.NodeList;
-
 public class Main {
 	public static void main(String[] args) {
 		// create a nodes
 		SinglyLinkedList.NodeList head=new SinglyLinkedList.NodeList(1);
-		SinglyLinkedList.NodeList nodeOne=new SinglyLinkedList.NodeList(2);
-		SinglyLinkedList.NodeList nodeTwo=new SinglyLinkedList.NodeList(3);
-		SinglyLinkedList.NodeList nodeThree=new SinglyLinkedList.NodeList(5);
-		SinglyLinkedList.NodeList nodeFour=new SinglyLinkedList.NodeList(6);
+		SinglyLinkedList.NodeList nodeOne=new SinglyLinkedList.NodeList(8);
+		SinglyLinkedList.NodeList nodeTwo=new SinglyLinkedList.NodeList(10);
+		SinglyLinkedList.NodeList nodeThree=new SinglyLinkedList.NodeList(16);
 		// link nodes
 		head.next=nodeOne;
 		nodeOne.next=nodeTwo;
 		nodeTwo.next=nodeThree;
-		nodeThree.next=nodeFour;
 		//logic of insert a node in a sorted linkedList
-		SinglyLinkedList.NodeList newNode=new SinglyLinkedList.NodeList(4);
-		NodeList current=head;
-		while(current!=null) {
-			if(current.data<newNode.data && current.next.data>newNode.data) {
-				newNode.next=current.next;
-				current.next=newNode;
+		SinglyLinkedList.NodeList newNode=new SinglyLinkedList.NodeList(10);
+		SinglyLinkedList.NodeList current=head;
+		SinglyLinkedList.NodeList temp=null;
+		if(newNode.data<=head.data) {
+			newNode.next=head;
+			head=newNode;
+		}
+		else {
+			while(current!=null && current.data<newNode.data) {
+				temp=current;
+				current=current.next;
 			}
-			current=current.next;
+			newNode.next=current;
+			temp.next=newNode;
 		}
 		// show reversed linked list
 		SinglyLinkedList.display(head);
