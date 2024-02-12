13ec03938ffec97c06b516c09d9d424e6a3eec17
Marwane Assou <marwane.assou@gmail.com>
2024-02-12 13:06:08 +0100

    implement circular linked List

---
+++ implement_circularLinkedList/src/implement_circularLinkedList/ListNode.java
@@ -0,0 +1,24 @@
+package implement_circularLinkedList;
+
+public class ListNode {
+	public static ListNode tail;
+	int data;
+	public ListNode next;
+	public ListNode(int data) {
+		this.data=data;
+	}
+	public static void insert(ListNode head,ListNode nodeOne,ListNode nodeTwo) {
+		nodeOne.next=nodeTwo;
+		nodeTwo.next=head;
+		tail=nodeTwo;
+	}
+	public static void display(ListNode head) {
+		int count=0;
+		while(count<4) {
+			System.out.println(head.data);
+			head=head.next;
+			count++;
+		}
+	}
+
+}
---
+++ implement_circularLinkedList/src/implement_circularLinkedList/Main.java
@@ -0,0 +1,17 @@
+package implement_circularLinkedList;
+
+public class Main {
+	public static void main(String[] args) {
+		// create ListNodes
+				ListNode head=new ListNode(1);
+				ListNode nodeOne=new ListNode(10);
+				ListNode nodeTwo=new ListNode(15);
+				ListNode nodeThree=new ListNode(65);
+				// link nodes
+				ListNode.insert(head,head, nodeOne);
+				ListNode.insert(head,nodeOne,nodeTwo);
+				ListNode.insert(head,nodeTwo, nodeThree);
+				// logic delete node in the start and in the end
+				ListNode.display(head);
+	}
+}
