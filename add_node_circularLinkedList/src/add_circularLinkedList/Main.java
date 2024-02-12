da31848a0b3f9b84a32a17f8eaa485126385a9d7
Marwane Assou <marwane.assou@gmail.com>
2024-02-12 14:32:07 +0100

    add node circular linkedList

---
+++ add_node_circularLinkedList/src/add_circularLinkedList/Main.java
@@ -0,0 +1,21 @@
+package add_circularLinkedList;
+
+public class Main {
+	public static void main(String[] args) {
+		// create ListNodes
+				ListNode head=new ListNode(1);
+				ListNode nodeOne=new ListNode(8);
+				ListNode nodeTwo=new ListNode(10);
+				ListNode nodeThree=new ListNode(16);
+				// link nodes
+				ListNode.insert(head,head, nodeOne);
+				ListNode.insert(head,nodeOne,nodeTwo);
+				ListNode.insert(head,nodeTwo, nodeThree);
+				// logic add new Node in the beginning
+				ListNode newNode=new ListNode(24);
+				ListNode.tail.next=newNode;
+				newNode.next=head;
+				head=newNode;
+				ListNode.display(head);
+	}
+}
