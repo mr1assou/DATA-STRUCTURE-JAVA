63cf6ea92342ac9b630ed7d4214e7a0d8fa3c857
Marwane Assou <marwane.assou@gmail.com>
2024-02-16 11:50:22 +0100

    iterative preloader binary Tree

---
+++ iterative_preloader_binaryTree/src/itertive_preloader_binaryTree/Main.java
@@ -0,0 +1,11 @@
+package itertive_preloader_binaryTree;
+
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		BinaryTree bt=new BinaryTree();
+		bt.createBinaryTree();
+		bt.iterativePreorder();
+	}
+}
---
+++ iterative_preloader_binaryTree/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module itertive_preloader_binaryTree {
+}
