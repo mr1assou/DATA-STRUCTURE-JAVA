239fea7e5ae9001b48ba457ddf5dac90e20a054a
Marwane Assou <marwane.assou@gmail.com>
2024-02-23 17:50:34 +0100

    insert an elemnt in max heap

--- maxPriorityQueue/src/maxPriorityQueue/MaxPQ.java
+++ maxPriorityQueue/src/maxPriorityQueue/MaxPQ.java
@@ -1,5 +1,7 @@
 package maxPriorityQueue;
 
+import java.util.Arrays;
+
 public class MaxPQ {
 	private int[] heap;
 	private int n;
@@ -13,9 +15,43 @@ public class MaxPQ {
 	public int size() {
 		return n;
 	}
+	public void insert(int x) {
+		if(n==heap.length-1) {
+			resize(heap.length*2);
+		}
+		n++;
+		heap[n]=x;
+		swim(n);
+	}
+	public void resize(int n) {
+		int temp[]=new int[n];
+		for(int i=0;i<heap.length;i++) {
+			temp[i]=heap[i];
+		}
+		heap=temp;
+	}
+	public void swim(int k) {
+		while(k>1 && heap[k/2]<heap[k]) {
+			int temp=heap[k/2];
+			heap[k/2]=heap[k];
+			heap[k]=temp;
+			k/=2;
+		}
+	}
+	public void display() {
+		for(int i=1;i<heap.length;i++) {
+			System.out.println(heap[i]);
+		}
+	}
 	public static void main(String[] args) {
 		MaxPQ pq=new MaxPQ(3);
-		System.out.println(pq.size());
-		System.out.println(pq.isEmpty());
+		pq.insert(9);
+		pq.insert(3);
+		pq.insert(6);
+		pq.insert(2);
+		pq.insert(1);
+		pq.insert(5);
+		pq.insert(4);
+		pq.display();
 	}
 }
