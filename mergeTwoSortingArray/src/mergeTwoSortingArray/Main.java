c6689567349fd6b82d6fabe68374c7fa797344d7
Marwane Assou <marwane.assou@gmail.com>
2024-03-03 21:03:29 +0100

    merge two sorting array

---
+++ mergeTwoSortingArray/src/mergeTwoSortingArray/Main.java
@@ -0,0 +1,37 @@
+package mergeTwoSortingArray;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		int []arr1= {2,5,7};
+		int []arr2= {3,4,9};
+		System.out.println(Arrays.toString(merge(arr1,arr2,arr1.length,arr2.length)));
+	}
+	public static int[] merge(int[] arr1,int[] arr2,int size1,int size2) {
+		int i=0,j=0,k=0;
+		int []arr3=new int[size1+size2];
+		while(i<size1 && j<size2) {
+			if(arr1[i]<arr2[j]) {
+				arr3[k]=arr1[i];
+				i++;
+			}
+			else {
+				arr3[k]=arr2[j];
+				j++;
+			}
+			k++;
+		}
+		//if j in the end so rest value of arr1 go to arr3
+		while(i<size1) {
+			arr3[k]=arr1[i];
+			i++;k++;
+		}
+		//if i in the end so rest value of arr2 go to arr3
+		while(j<size2) {
+			arr3[k]=arr2[j];
+			j++;k++;
+		}
+		return arr3;
+	}
+}
