94b7278501d340cf29057a27b1536146739fe421
Marwane Assou <marwane.assou@gmail.com>
2024-03-13 18:20:08 +0000

    valid parentheses

--- validParentheses/src/validParentheses/Main.java
+++ validParentheses/src/validParentheses/Main.java
@@ -2,17 +2,22 @@ package validParentheses;
 import java.util.Stack;
 public class Main {
 	public static void main(String[] args) {
-		String s="}(";
+		String s="()";
 		System.out.println(isValid(s));
 	}
 	public static boolean isValid(String s) {
-		//if(s.length()%2!=0) return false;
 		Stack<Character>stack=new Stack<>();
 		for(int i=0;i<s.length();i++) {
 			if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{') {
 				stack.push(s.charAt(i));
 			}
-			else if(!stack.isEmpty() && ((s.charAt(i)==')' && stack.peek()=='(') || (s.charAt(i)=='}' && stack.peek()=='{') || (s.charAt(i)==']' && stack.peek()=='['))) {
+			else if(!stack.isEmpty() && ((s.charAt(i)==')' && stack.peek()=='('))) {
+				stack.pop();
+			}
+			else if(!stack.isEmpty() && (s.charAt(i)=='}' && stack.peek()=='{')) {
+				stack.pop();
+			}
+			else if(!stack.isEmpty() && (s.charAt(i)==']' && stack.peek()=='[')) {
 				stack.pop();
 			}
 			else {
