38c4dd2ec76f6b4b44c9e15afa2ff5cb4760fb87
Marwane Assou <marwane.assou@gmail.com>
2024-03-15 12:17:42 +0000

    remove element

---
+++ leetCodeRemoveElements/src/leetCodeRemoveElements/Main.java
@@ -0,0 +1,18 @@
+package leetCodeRemoveElements;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] nums= {0,1,2,2,3,0,4,2};
+		int val=2;
+		int j=0;
+	    for(int i=0;i<nums.length;i++){
+	    	if(nums[i]!=val){
+	            nums[j]=nums[i];
+	            j++;
+	        }
+	    }
+	    System.out.println(Arrays.toString(nums));
+	}
+}
