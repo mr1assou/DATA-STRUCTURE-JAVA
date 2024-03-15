530dd515a10df2985496c5a20673e4694ff9bfbb
Marwane Assou <marwane.assou@gmail.com>
2024-03-15 21:54:06 +0000

    leetCode plus one

---
+++ plusOne/src/plusOne/Main.java
@@ -0,0 +1,23 @@
+package plusOne;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] arr= {1,4,9,9};
+		System.out.println(Arrays.toString(plusOne(arr)));
+	}
+	public static int[] plusOne(int[] digits) {
+		int n=digits.length;
+		for(int i=n-1;i>=0;i--) {
+			if(digits[i]<9) {
+				digits[i]++;
+				return digits;
+			}
+			digits[i]=0;
+		}
+		int[] new_number=new int[n+1];
+		new_number[0]=1;
+		return new_number;
+	}
+}
