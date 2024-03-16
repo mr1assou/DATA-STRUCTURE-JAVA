6a4da147a0dffbc8c7a2144b37011ba574f70aef
Marwane Assou <marwane.assou@gmail.com>
2024-03-16 14:07:37 +0000

    add binary

---
+++ leetCodeAddBinary/src/leetCodeAddBinary/Main.java
@@ -0,0 +1,23 @@
+package leetCodeAddBinary;
+public class Main {
+	public static void main(String[] args) {
+		addBinary("11","1");
+	}
+	public static void addBinary(String str1,String str2) {
+		StringBuilder sb=new StringBuilder();
+		int carry=0;
+		int i=str1.length()-1;
+		int j=str2.length()-1;
+		while(i>=0 || j>=0) {
+			int sum=carry;
+			if(i>=0) sum+=str1.charAt(i)-'0';
+			if(j>=0) sum+=str2.charAt(j)-'0';
+			carry=sum/2;
+			sb.append(sum%2);
+			i--;
+			j--;
+		}
+		if(carry!=0) sb.append(carry);
+		System.out.println(sb.reverse());
+	}
+}
