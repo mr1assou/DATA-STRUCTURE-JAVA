71f3baae6df1d1e0f967e2bfde3c1463d9c93af8
Marwane Assou <marwane.assou@gmail.com>
2024-03-23 01:45:33 +0000

    leetCode mximum Depth binary ree

--- leetCodeMximumDepthOfbinaryTree/src/maximumDepthOfbinaryTree/Main.java
+++ leetCodeMximumDepthOfbinaryTree/src/maximumDepthOfbinaryTree/Main.java
@@ -1,5 +1,7 @@
 package maximumDepthOfbinaryTree;
 
 public class Main {
-
+	public static void main(String[] args) {
+		// see the solution in Solution class
+	}
 }
--- leetCodeMximumDepthOfbinaryTree/src/maximumDepthOfbinaryTree/Solution.java
+++ leetCodeMximumDepthOfbinaryTree/src/maximumDepthOfbinaryTree/Solution.java
@@ -1,5 +1,19 @@
 package maximumDepthOfbinaryTree;
 
 public class Solution {
-
+	public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        return maxDepth(root,1,1);
+    }
+    public int maxDepth(TreeNode root,int max,int level){
+        if(root==null){
+            return max;
+        }
+        if(level>max){
+            max=level;
+        }
+        max=maxDepth(root.left,max,level+1);
+        max=maxDepth(root.right,max,level+1);
+        return max;
+    }
 }
