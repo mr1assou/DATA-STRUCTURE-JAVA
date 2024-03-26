efaee5b902f5ab1833c58fc3aaf4d99da49cff15
Marwane Assou <marwane.assou@gmail.com>
2024-03-26 20:22:39 +0000

    triangle pascal

--- leetCodeTrianglePascal/src/leetCodeTrianglePascal/Main.java
+++ leetCodeTrianglePascal/src/leetCodeTrianglePascal/Main.java
@@ -1,5 +1,7 @@
 package leetCodeTrianglePascal;
 
 public class Main {
-
+	public static void main(String[] args) {
+		//see the implementation in solution file
+	}
 }
--- leetCodeTrianglePascal/src/leetCodeTrianglePascal/Solution.java
+++ leetCodeTrianglePascal/src/leetCodeTrianglePascal/Solution.java
@@ -1,5 +1,23 @@
 package leetCodeTrianglePascal;
-
+import java.util.List;
+import java.util.ArrayList;
 public class Solution {
-
+	 public List<List<Integer>> generate(int numRows) {
+	        List<List<Integer>> triangle=new ArrayList<>();
+	        if(numRows==0) return triangle;
+	        List<Integer> firstRow=new ArrayList<>();
+	        firstRow.add(1);
+	        triangle.add(firstRow);
+	        for(int i=1;i<numRows;i++){
+	            List<Integer> prevRow=triangle.get(i-1);
+	            List<Integer> row=new ArrayList<>();
+	            row.add(1);
+	            for(int j=1;j<i;j++){
+	                row.add(prevRow.get(j)+prevRow.get(j-1));
+	            }
+	            row.add(1);
+	            triangle.add(row);
+	        }
+	        return triangle;
+	    }
 }
