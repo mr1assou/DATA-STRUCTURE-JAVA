56186908d475020b0e4a0412ec4997dd82042c7d
Marwane Assou <marwane.assou@gmail.com>
2024-04-11 08:50:03 +0000

    majority elemnt

--- leetcodeMajorityElement/src/leetcodeMajorityElement/Solution.java
+++ leetcodeMajorityElement/src/leetcodeMajorityElement/Solution.java
@@ -1,5 +1,21 @@
 package leetcodeMajorityElement;
 
-public class Solution {
 
+public class Solution {
+	public int majorityElement(int[] nums) {
+		int majority=nums[0],votes=1;
+        for(int i=1;i<nums.length;i++){
+            if(votes==0){
+                votes++;
+                majority=nums[i];
+            }
+            else if(majority==nums[i]){
+                votes++;
+            }
+            else{
+                votes--;
+            }
+        }
+        return majority;
+    }
 }
